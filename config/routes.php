e6b532ef1df8b2a2511841b019e4615b139e0d0d
TheHerbin <theo.herbin59710@outlook.fr>
2020-09-27 23:18:44 +0000

    routes protégées , reste a faire les boutons d'accès clients

--- config/routes.php
+++ config/routes.php
@@ -10,28 +10,58 @@ function getPage($db){
  $lesPages['maintenance'] = "maintenanceControleur;0";
  $lesPages['connexion'] = "connexionControleur;0";
  $lesPages['deconnexion'] = "deconnexionControleur;0";
- $lesPages['utilisateur'] = "utilisateurControleur;0";
- $lesPages['creerObus'] = "creerObusControleur;0";
- $lesPages['stock'] = "stockControleur;0";
- $lesPages['obus'] = "obusControleur;0";
+ $lesPages['utilisateur'] = "utilisateurControleur;Administrateur";
+ $lesPages['creerObus'] = "creerObusControleur;Client;Administrateur";
+ $lesPages['stock'] = "stockControleur;Administrateur;Client";
+ $lesPages['obus'] = "obusControleur;Administrateur;Client";
 
 
- if($db!=null){
-
- if (isset($_GET['page'])){
+ if ($db!=null){
+    if (isset($_GET['page'])){
     $page = $_GET['page'];
     }
     else{
     $page = 'accueil';
-}
-if (isset($lesPages[$page])){
-$contenu = $lesPages[$page];
-}
-else{
+    }
+    if (!isset($lesPages[$page])){
+    // Nous rentrons ici si cela n'existe pas, ainsi nous redirigeons l'utilisateur sur la page d'accueil
+    $page = 'accueil';
+    }
+   
+    $explose = explode(";",$lesPages[$page]);
+    // Nous découpons la ligne du tableau sur le
+    //caractère « ; » Le résultat est stocké dans le tableau $explose
+   
+    $role = $explose[1]; // Le rôle est dans la 2ème partie du tableau $explose
+   
+    if ($role != 0){ // Si mon rôle nécessite une vérification
+    if(isset($_SESSION['login'])){ // Si je me suis authentifié
+    if(isset($_SESSION['role'])){ // Si j’ai bien un rôle
+    if($role!=$_SESSION['role']){ // Si mon rôle ne correspond pas à celui qui est nécessaire 
+        //pour voir la page
+    $contenu = 'accueilControleur'; // Je le redirige vers l’accueil, car il n’a pas le bon rôle
+    }
+    else{
+    $contenu = $explose[0]; // Je récupère le nom du contrôleur, car il a le bon rôle
+    }
+    }
+    else{
+    $contenu = 'accueilControleur';;
+    }
+    }
+    else{
+    $contenu = 'accueilControleur';; // Page d’accueil, car il n’est pas authentifié
+    }
+    }else{
+    $contenu = $explose[0]; // Je récupère le contrôleur, car il n’a pas besoin d’avoir un rôle
+    }
+    }
+   else{
     $contenu = $lesPages['maintenance'];
    }
-
+   
+   
 return $contenu;
 }
-}
+
 ?>
